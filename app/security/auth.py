d3115c6285911149252e7f0fb8af87de56928024
RhuanSantos135 <rhuansantos507@gmail.com>
2025-08-22 21:35:41 -0300

    feat(auth): adicioando seguraça na aplicação

--- app/security/auth.py
+++ app/security/auth.py
@@ -1,4 +1,10 @@
 from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+from .schema import Token
+from jwt import encode
+from zoneinfo import ZoneInfo
+from app.conf.conf import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+
 
 context =  PasswordHash.recommended()
 
@@ -7,3 +13,12 @@ def get_hash(password: str):
 
 def verify_hash(password: str , password_hash: str):
     return context.verify(password, password_hash)
+
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({'exp': expire})
+    encode_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
